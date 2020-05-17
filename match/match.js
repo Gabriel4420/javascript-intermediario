573cfe3db2134dac7187702e9809f53d65582d03
= <gabriel_rodrigues_perez@hotmail.com>
2020-05-17 19:40:35 -0300

    desafio_prova

--- match/match.js
+++ match/match.js
@@ -5,3 +5,10 @@ const txt_non_structered = 'meu cpf é 123.456.569-22';
 const regex = new RegExp('[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}');
 
 console.log(txt_non_structered.match(regex))
+
+//desafio prova 
+
+let partes = ['ombros', 'joelhos']; 
+let letra = ['cabeça', ...partes, 'e', 'dedos'];
+
+console.log(letra);
