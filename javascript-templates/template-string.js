69aaff0c9632c7f07a9a729c1fa5ad19320cce67
= <gabriel_rodrigues_perez@hotmail.com>
2020-05-17 18:35:47 -0300

    template strings

---
+++ javascript-templates/template-string.js
@@ -0,0 +1,12 @@
+// var coloca muita coisa em memória
+let nome = 'Gabriel';
+let sobrenome = 'Rodrigues Perez';
+let profissao = 'Frellancer WP,JS';
+// sem template strings
+console.log('Olá, eu sou '+nome+' '+sobrenome+'\n minha profissão é '+profissao);
+
+// com template strings
+
+console.log(`Ola, meu nome é ${nome +' '+ sobrenome} e minha profissão é ${profissao} `)
+
+console.log(`O resultado de 1+1 é : ${1+1}`)
